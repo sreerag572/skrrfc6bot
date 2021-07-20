3eed8410b073e27ff4a934d123ba1bc860d5af82
sreerag572 <85470408+sreerag572@users.noreply.github.com>
2021-07-20 11:52:40 +0530

    Update commands.py

--- bot/plugins/commands.py
+++ bot/plugins/commands.py
@@ -28,14 +28,14 @@ async def start(bot, update):
             await update.reply_cached_media(
                 file_id,
                 quote=True,
-                caption = caption,
+                caption = @royalfilimclub123,
                 parse_mode="html",
                 reply_markup=InlineKeyboardMarkup(
                     [
                         [
                             InlineKeyboardButton
                                 (
-                                    'Developers', url="https://t.me/CrazyBotsz"
+                                    '🔰 Channel 🔰', url="https://t.me/royalfilimclub123       "
                                 )
                         ]
                     ]
